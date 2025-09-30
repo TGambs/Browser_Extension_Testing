4cc19e98b5bcb0bf17225b44a974155dbea9480d
Tommaso Gambro <tg2016@hw.ac.uk>
2025-09-30 13:20:51 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 console.log("This is a popup!");
 
 // using the npm installed version of KYBER
-const kyber = require("crystals-kyber");
+import kyber from crystals-kyber;
 
 // generate a public key
 let pk = kyber.KeyGen512();
