f435754aefd85a70e92dd7058ad892da0d92a397
Tommaso Gambro <tg2016@hw.ac.uk>
2025-09-30 13:17:44 +0100

    Started using KYBER
    installed via npm

--- popup.js
+++ popup.js
@@ -1,1 +1,9 @@
-console.log('This is a popup!');
+console.log("This is a popup!");
+
+// using the npm installed version of KYBER
+const kyber = require("crystals-kyber");
+
+// generate a public key
+let pk = kyber.KeyGen512();
+// print it to console
+console.log(pk);
