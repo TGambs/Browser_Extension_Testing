6775654ddc744844be02f3194a205813ab050aa2
Tommaso Gambro <tg2016@hw.ac.uk>
2025-12-23 00:13:28 +0000

    Kyber working in extension

--- src/background.js
+++ src/background.js
@@ -7,15 +7,6 @@ console.log("\n--- Background.js loading ---");
 import { MlKem768 } from "crystals-kyber-js";
 const recipient = new MlKem768();
 console.log(recipient);
-/*
-import kyber from "../src/kyber.js";
-
-console.log("Kyber loaded from background:", kyber);
-//generate a public key
-let pk = kyber.KeyGen512();
-//print it to console
-console.log(pk);
-*/
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   console.log("Message received in background:", request);
